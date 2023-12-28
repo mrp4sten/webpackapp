9917b5f56f2f8c3a2ba67e0f866c11505d797a0a
mrp4sten <mauricio.pasten.martinez@gmail.com>
2023-12-27 21:56:48 -0600

    Add main.scss file with styles

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { v4 as uuidv4 } from 'uuid'
 import generateJoke from './generateJoke'
+import './styles/main.scss'
 
 console.log('generateJoke :>> ', generateJoke())
 console.log('uuidv4 :>> ', uuidv4())
