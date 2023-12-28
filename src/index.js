6aec6c1530351cb8f0ad08ea42ba408a7a776bef
mrp4sten <mauricio.pasten.martinez@gmail.com>
2023-12-27 22:32:07 -0600

    Add functionality to generate jokes from an API

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import { v4 as uuidv4 } from 'uuid'
 import laughing from './assets/laughing.svg'
 import generateJoke from './generateJoke'
 import './styles/main.scss'
@@ -6,5 +5,5 @@ import './styles/main.scss'
 const laughImg = document.getElementById('laughImg')
 laughImg.src = laughing
 
-console.log('generateJoke :>> ', generateJoke())
-console.log('uuidv4 :>> ', uuidv4())
+const jokeBtn = document.getElementById('jokeBtn')
+jokeBtn.addEventListener('click', generateJoke)
