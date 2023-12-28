be622853d51382c45a39387242e67d8ef8205a23
mrp4sten <mauricio.pasten.martinez@gmail.com>
2023-12-27 20:02:56 -0600

    Add uuid dependency and update console logs

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
+import { v4 as uuidv4 } from 'uuid'
 import generateJoke from './generateJoke'
-console.log('generateJoke :>> ', generateJoke)
+
+console.log('generateJoke :>> ', generateJoke())
+console.log('uuidv4 :>> ', uuidv4())
